c5b9d10459176e8558c78ac5916c82230a3c760f
Karthik Gupta <karthikts.gupta@gmail.com>
2021-09-26 22:30:03 +0530

    Update SpringrestApplication.java

--- src/main/java/com/karthik/springrest/SpringrestApplication.java
+++ src/main/java/com/karthik/springrest/SpringrestApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class SpringrestApplication {
 
+	/* application start up */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestApplication.class, args);
 	}
